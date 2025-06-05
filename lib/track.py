ff349cdee7ff4310d83767b99af42460cd00db78
Sean McCaughey <sean.mccaughey@protonmail.com>
2025-06-05 15:06:51 +0100

    finished challenge

--- lib/track.py
+++ lib/track.py
@@ -1,7 +1,7 @@
 class tracker:
     def __init__(self):
         self.track_list = []
-    def add_track(self,track):
+    def add_track(self,track = ""):
         self.track_list.append(track)
     def history(self):
         return self.track_list
--- tests/test_track.py
+++ tests/test_track.py
@@ -6,3 +6,15 @@ def test_one_track_is_added():
     Spotify = tracker()
     Spotify.add_track("Despacito")
     assert Spotify.history() == ["Despacito"]
+
+def test_no_track_is_added():
+    Soundcloud = tracker()
+    Soundcloud.add_track()
+    assert Soundcloud.history() == [""]
+
+def test_three_tracks_are_added():
+    AppleMusic = tracker()
+    AppleMusic.add_track("song1")
+    AppleMusic.add_track("song2")
+    AppleMusic.add_track("song3")
+    assert AppleMusic.history() == ["song1", "song2", "song3"]
